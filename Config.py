510181e361e790056ac7b267b9d8c6a3acf5c173
lihao <2197248717@qq.com>
2023-11-18 16:43:58 +0800

    kube-config

--- Config.py
+++ Config.py
@@ -3,8 +3,8 @@ import time
 
 class Config:
     def __init__(self):
-        self.namespace = 'bookinfo'
-        self.nodes = {
+        self.namespace = 'bookinfo'  # 命名空间（默认）
+        self.nodes = {  # 节点名
             'master': 'izbp193ioajdcnpofhlr1hz',
             'cloud-worker1': 'izbp1gwb52uyj3g0wn52lez',
             'cloud-worker2': 'izbp1gwb52uyj3g0wn52lfz',
@@ -14,18 +14,18 @@ class Config:
             'edge-worker3': 'server-3',
             'edge-worker4': 'dell2018',
         }
-        self.svcs = set()
-        self.pods = set()
+        self.svcs = set()  # 服务
+        self.pods = set()  # 实例
 
-        self.interval = 10 * 60
+        self.interval = 10 * 60  # 每次收集数据的时间（10min）
         # duration related to interval
         self.duration = self.interval
         self.start = int(round((time.time() - self.duration)))
         self.end = int(round(time.time()))
 
         # prometheus
-        self.prom_range_url = "http://47.99.240.112:31444/api/v1/query_range"
-        self.prom_range_url_node = "http://47.99.240.112:31222/api/v1/query_range"
+        self.prom_range_url = "http://47.99.240.112:31444/api/v1/query_range"  # istio支持
+        self.prom_range_url_node = "http://47.99.240.112:31222/api/v1/query_range"  # 原生Prometheus
         self.prom_no_range_url = "http://47.99.240.112:31444/api/v1/query"
         self.step = 5
 
@@ -35,7 +35,7 @@ class Config:
         self.limit = 100000
 
         # kubernetes
-        self.k8s_config = 'local-config'
+        self.k8s_config = 'config.yaml'  # kubernetes配置文件地址
 
         # concurrency set
-        self.user = '20user-hybrid'
+        self.user = '10user-hybrid'  # 测试数据名
